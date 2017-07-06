6b35d19a39b89362cb94a7bb7c0f183c054d6f79
Anatoly Russkih <titanofdarkness@gmail.com>
2017-07-06 13:37:26 +1000

    Some small optimization CustomHtmlWordWrap.

--- inc/customhtmlwordwrap.h
+++ inc/customhtmlwordwrap.h
@@ -8,23 +8,20 @@ class CustomHtmlWordWrap
 public:
     static QString ProcessString(const QString& longString, const int& maxCharCount = MAX_CHARACTERS_IN_LINE)
     {
-        QString result = QString("<p style=\"white-space:pre\">");
+        QString result;
         const auto reserve_chars = longString.length() < maxCharCount ? longString.length() + 50 : ((longString.length() / maxCharCount) + 1) * maxCharCount;
         result.reserve(reserve_chars);
+        result.append("<p style=\"white-space:pre\">");
         size_t counter = 0;
         bool is_tag = false;
         for (int i = 0; i < longString.length(); ++i) {
             const auto& ch = longString[i];
             if (ch == "<") {
                 is_tag = true;
-            } else {
-                if (counter > maxCharCount) {
-                    if (ch == " " && is_tag == false) {
-                        result.append("<br />");
-                        counter = 0;
-                        continue;
-                    }
-                }
+            } else if (counter > maxCharCount && ch == " " && !is_tag) {
+                result.append("<br />");
+                counter = 0;
+                continue;
             }
             result.append(ch);
             if (!is_tag) {
